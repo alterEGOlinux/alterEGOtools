48b760f365706629732b39d25f8fb97a4e1cbc8a
Pascal Malouin <pascal.malouin@gmail.com>
2021-10-19 06:57:59 -0400

    Added setup.py

--- alterEGOtools/vmutils.py
+++ alterEGOtools/vmutils.py
@@ -4,7 +4,7 @@
 ##                                                                           ##
 ## alterEGOtools/vmutils.py                                                  ##
 ##   created        : 2021-10-18 21:10:53 UTC                                ##
-##   updated        : 2021-10-18 21:11:03 UTC                                ##
+##   updated        : 2021-10-19 10:50:45 UTC                                ##
 ##   description    : Virtual machines module                                ##
 ## _________________________________________________________________________ ##
 
@@ -12,10 +12,6 @@ import subprocess
 import shlex
 
 def detect_vm():
-
-    #### Use `$ systemd-detect-virt`
-    #... If VirtualBox will return 'oracle'.
-    #... If not in VM, will return 'none'.
 
     detect = subprocess.run(shlex.split(f"systemd-detect-virt"), stdout=subprocess.PIPE)
 
---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+## { alterEGO Linux: "Open the vault of knowledge" } ----------------------- ##
+##                                                                           ##
+## setup.py                                                                  ##
+##   created        : 2021-10-19 10:54:36 UTC                                ##
+##   updated        : 2021-10-19 10:54:45 UTC                                ##
+##   description    : Setup file for alterEGOtools module.                   ##
+## _________________________________________________________________________ ##
+
+
+from distutils.core import setup
+
+setup(name='alterEGOtools',
+      version='1.0',
+      description='Tools to build alterEGO Linux',
+      author='Pascal Malouin (fantomH)',
+      author_email='pascal.malouin@gmail.com',
+      url='#',
+      packages=['alterEGOtools'],
+     )
+
